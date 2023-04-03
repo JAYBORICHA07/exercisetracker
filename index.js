9c0bc6bc6866ee8ad0c4475ffe3cf0ec033da38c
Jay Boricha <jayboricha707@gmail.com>
2023-04-03 19:41:17 +0530

    all test case passed

--- index.js
+++ index.js
@@ -1,143 +1,164 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require ('mongoose')
-const bodyParser = require('body-parser')
-const Schema = mongoose.Schema;
-const {urlencoded} = require('express')
-require('dotenv').config()
-
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const { urlencoded } = require("express");
 
 const userSchema = mongoose.Schema(
-  {username : {
-    type : "string",
-    unique : true,
-  }},
-  {versionKey : false}
+  {
+    username: {
+      type: "string",
+      unique: true,
+      require: true,
+    },
+  },
+  { versionKey: false }
 );
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 const exerciseSchema = mongoose.Schema(
-  {username : {
-    type : "string",
-    unique : true,
+  {
+    userId: {
+      type: "string",
+      require: true,
+    },
+    username: {
+      type: "string",
+      require: true,
+    },
+    date: {
+      type: Date,
+      require: true,
+    },
+    duration: {
+      type: Number,
+      require: true,
+    },
+    description: {
+      type: "string",
+      require: true,
+    },
   },
-  description : "string",
-  duration : "number",
-  date : "string",
-  userId : "string"
-  },
-  {versionKey : false}
+  { versionKey: false }
 );
 
-const Exercise = mongoose.model('Exercise', exerciseSchema);
+const Exercise = mongoose.model("Exercise", exerciseSchema);
 
+mongoose
+  .connect(
+    "mongodb+srv://jayboricha707:OHnPpvvEGYJXvnQg@cluster0.ez6lqeq.mongodb.net/freecodecamp?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    console.log("Database Connected Succesfully!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-  console.log("Database Connected Succesfully!")
-}).catch((err) => {
-  console.log(err);
-})
-
-
-app.use(express.json())
-app.use(express.urlencoded({extended : true}));
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get('/api/users', async (req, res) =>{
+app.get("/api/users", async (req, res) => {
   const users = await User.find();
   res.send(users);
-})
+});
 
-app.post('/api/users',  async (req,res) => {
-  const username =  req.body.username;
-  // const foundUser = await User.findOne({ username });
-  // if(foundUser){
-    // res.json(foundUser)
-  // }
+app.post("/api/users", async (req, res) => {
+  const username = req.body.username;
+  const foundUser = await User.findOne({ username });
+  if (foundUser) {
+    res.json(foundUser);
+  }
   const user = await User.create({
     username,
-  })
-  res.json(user)
-})
+  });
+  res.json(user);
+});
 
-app.get('/api/users/:_id/logs', async (req,res) => {
-  let {from, to ,limit} = req.query;
+app.get("/api/users/:_id/logs", async (req, res) => {
+  let { from, to,limit } = req.query;
   const userId = req.params._id;
   const foundUser = await User.findById(userId);
-  if(!foundUser){
-    res.json({ error :" Invalide user or Id"})
+  if (!foundUser) {
+    res.json({ error: " Invalid user or Id" });
   }
 
-  let filter = {userId}
-  let dateFilter = {}
-  if(from){
-    dateFilter['$gte'] = new Date(from);
-  }
-  if(to){
-    dateFilter['$lte'] = new Date(to);
-  }
-  if(from || to){
-    filter.date = dateFilter;
-  }
-  if(!limit){
-    limit = 100;
-  }
-  let exercises = await Exercise.find(filter).limit(limit)
-    exercises = exercises.map((exercise) => {
-    return {
-      discription : exercise.description,
-      duration : exercise.duration,
-      date : exercise.date,
-    }
-  });
-  
-  res.json({
-    username : foundUser.username,
-    count : exercises.length,
-    _id : userId,
-    log : exercises
-  })
-})
-
-app.post('/api/users/:_id/exercises', async (req,res) =>{
-  const userId = req.body[":_id"];
-  let {description, duration, date} = req.body;
-  const foundUser = await User.findById(userId);
-  if(!date){
-    date = new Date();
+  let filter = { userId };
+  // let dateFilter = {};
+  // if (from) {
+  //   dateFilter["$gte"] = new Date(from);
+  // }
+  // if (to) {
+  //   dateFilter["$lte"] = new Date(to);
+  // }
+  // if (from || to) {
+  //   filter.date = dateFilter;
+  // }
+  let exercises 
+  if (!limit) {
+    exercises = await Exercise.find(filter);
   }else{
+  exercises = await Exercise.find(filter).limit(limit).exec();
+  }
+  exercises = exercises.map((exercise) => { 
+    return {
+      description: exercise.description,
+      duration: exercise.duration,
+      date: exercise.date.toDateString(),
+    };
+  });
+const responseObj = {
+  username: foundUser.username,
+  count: exercises.length,
+  _id: userId,
+  log: exercises,
+}
+  // console.log(responseObj)
+  res.json(responseObj);
+});
+
+app.post("/api/users/:_id/exercises", async (req, res) => {
+  const userId = req.params["_id"];
+  let { description, duration, date } = req.body;
+  const foundUser = await User.findById(userId);
+  if (!date) {
+    date = new Date();
+  } else {
     date = new Date(date);
   }
-  if(!foundUser){
-    res.json({ error :" Invalide user or Id"})
+  if (!foundUser) {
+    res.json({ error: " Invalided user or Id" });
   }
-
+  console.log(date);
   await Exercise.create({
-    username : foundUser.username,
-    description,
-    duration,
-    date,
     userId,
-  })
-  
-  res.send({
-    username : foundUser.username,
-    description,
+    username: foundUser.username,
+    date,
     duration,
-    date : date.toDateString(),
-    _id : userId,
-    
+    description,
   });
-})
 
+
+  const user = {
+    _id: userId,
+    username: foundUser.username,
+    date: date.toDateString(),
+    duration : Number(duration),
+    description,
+  }
+  // console.log(user)
+  res.json(user);
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
